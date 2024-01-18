ceb19d61e24108a22c82d9aa7e1ab3827e72285c
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-01-18 18:46:10 -0500

    Add files via upload

---
+++ listaestudiantes/listaestudiantes.js
@@ -0,0 +1,6 @@
+'use strict'
+let pregunta = prompt("Ingresa el nombre del estudiante");
+let students = ["Juan","Jose","Pedro","Maria","Alejandra"];
+alert(students.includes (pregunta));
+
+//Mostrar en un alert: "Si se encuentra" o "No se encuentra"
