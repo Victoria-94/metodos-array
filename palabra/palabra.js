1c3676ceb14e583bb5a6cf966c239c76becdb012
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-01-18 18:45:57 -0500

    Add files via upload

---
+++ palabra/palabra.js
@@ -0,0 +1,5 @@
+'use strict'
+// Separar por palabras y mostrar cuantas palabras tiene la siguiente frase:
+let phrase = 'La programación web es fundamental para el progres'
+let palabra= phrase.split(' ');
+alert(palabra);
